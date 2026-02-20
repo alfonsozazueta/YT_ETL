8fd260f8b0aaa110e07d29478ef0c4f0fbf2cdce
Alfonso Zazueta <alfonsozazuetab@gmail.com>
2026-02-20 12:24:29 -0600

    Wrote first unit tests

--- dags/main.py
+++ dags/main.py
@@ -1,8 +1,13 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime,timedelta
+
+
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
 from datawarehouse.dwh import staging_table, core_table
+from dataquality.soda import yt_elt_data_quality
+
+
 local_tz = pendulum.timezone("America/Mexico_City")
 
 default_args ={
@@ -22,10 +27,29 @@ with DAG(
     description = 'DAG to process JSON file and insert data into both staging and core schema',
     schedule='0 15 * * *',
     catchup = False
-) as dag:
+) as dag_update:
     update_staging = staging_table()
     update_core = core_table()
     
     
     #define depends
     update_staging >> update_core
+    
+    
+staging_schema = 'staging'
+core_schema = 'core'
+
+with DAG(
+    dag_id = 'data_quality',
+    default_args = default_args,
+    description = 'DAG to check the data quality on both layers in db',
+    schedule='0 16 * * *',
+    catchup = False
+) as dag_update:
+    
+    soda_validate_staging = yt_elt_data_quality(staging_schema)
+    soda_validate_core = yt_elt_data_quality(core_schema)
+    
+    
+    #define depends
+    soda_validate_staging >> soda_validate_core
